59045c8b92eec24a5883713b11fa08837429e2cc
Dino Morelli <dino@ui3.info>
2007-04-24 03:06:12 +0000

    Added info about naming scheme to usage output.

--- src/Photoname/Opts.hs
+++ src/Photoname/Opts.hs
@@ -64,6 +64,14 @@ usageText = (usageInfo header options) ++ "\n" ++ footer
          , "Options:"
          ]
       footer = init $ unlines
-         [ ""
+         [ "I have been through several picture naming and directory layout debacles over"
+         , "the years. My most recent decision was to try the scheme detailed by one"
+         , "Calvin Hass on his Impulse Adventure site"
+         , "(http://www.impulseadventure.com/photo/flow-name-file.html)"
+         , ""
+         , "The basic idea is this: If I have a file named (by my Canon camera)"
+         , "img_1582.jpg and which was shot on 2007-04-23, this file would be linked"
+         , "to or moved to this path: <top-level>/2007/2007-04-23/20070423_582.jpg"
+         , ""
          , "Version 001  2007-Apr-23  Dino Morelli <dino@ui3.info>"
          ]
