72a98b32d5013f48e7c9e8bac4d1b1b578d379d9
Dino Morelli <dino@ui3.info>
2007-04-24 01:32:57 +0000

    Added licensing header comment to source files.

--- src/Photoname/Opts.hs
+++ src/Photoname/Opts.hs
@@ -1,14 +1,14 @@
 {-
    Copyright 2007 Dino Morelli
 
-   This file is part of storylen
+   This file is part of photoname
 
-   storylen is free software; you can redistribute it and/or modify
+   photoname is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
 
-   storylen is distributed in the hope that it will be useful,
+   photoname is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
