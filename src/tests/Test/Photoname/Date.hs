fb06c36f2057544abcba9366ff18f64b5de99f12
Dino Morelli <dino@ui3.info>
2021-10-26 09:50:30 -0400

    Added Semigroup and Monoid law tests for PhDate
    This date type is in instance of Semigroup and Monoid. We now have
    quickcheck property tests verifying these implementations satisfy the
    laws.

--- src/tests/Test/Photoname/Date.hs
+++ src/tests/Test/Photoname/Date.hs
@@ -1,3 +1,6 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Test.Photoname.Date
   ( tests
   )
@@ -8,6 +11,7 @@ import Data.Time.LocalTime
 import Photoname.Date ( PhDate (..), parseExifDate, parseSignalDate )
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
 
 
 expectedLocalTime :: LocalTime
@@ -17,6 +21,13 @@ expectedLocalTime = LocalTime (fromGregorian 2021 10 04)
 
 tests :: TestTree
 tests = testGroup "Photoname.Date"
+  [ parsingTests
+  , propsPhDate
+  ]
+
+
+parsingTests :: TestTree
+parsingTests = testGroup "parsing tests"
   [ testCase "parse a datetime in EXIF format" $
       ExifDate expectedLocalTime @=?
         (parseExifDate $ Just "2021:10:04 17:29:49")
@@ -26,4 +37,26 @@ tests = testGroup "Photoname.Date"
   , testCase "parse a date from a signal jpg filename with more hyphens" $
       FilenameDate expectedLocalTime @=?
         parseSignalDate "some/directory/signal-2021-10-04-17-29-49-942.jpg"
+  ]
+
+
+instance Arbitrary PhDate where
+  arbitrary = do
+    oneof
+      [ return $ ExifDate expectedLocalTime
+      , return $ FilenameDate expectedLocalTime
+      , return NoDateFound
+      ]
+
+propsPhDate :: TestTree
+propsPhDate = testGroup "testing the Semigroup and Monoid properties of PhDate"
+  [ testProperty "Semigroup associativity  x <> (y <> z) == (x <> y) <> z" $
+    \(x :: PhDate) (y :: PhDate) (z :: PhDate) ->
+      x <> (y <> z) == (x <> y) <> z
+  , testProperty "Monoid right identity  x <> mempty == x" $
+    \(x :: PhDate) -> x <> mempty == x
+  , testProperty "Monoid left identity  mempty <> x == x" $
+    \(x :: PhDate) -> mempty <> x == x
+  , testProperty "Monoid concatenation  mconcat xs == foldr (<>) mempty xs" $
+    \(xs :: [PhDate]) -> mconcat xs == foldr (<>) mempty xs
   ]
