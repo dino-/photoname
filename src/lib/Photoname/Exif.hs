8abb6e2ea5997b97788d2d41fd517e2b614ccafe
Dino Morelli <dino@ui3.info>
2022-07-26 07:32:05 -0400

    Switched back to the Haskell exif library
    This was an attempt to explore support for the WEBP graphics format by
    switching back to the Haskell exif library which is a wrapper around
    libexif. There is no support at this time but these changes may prove to
    be useful in the future.

--- src/lib/Photoname/Exif.hs
+++ src/lib/Photoname/Exif.hs
@@ -5,32 +5,52 @@ module Photoname.Exif
   )
   where
 
-import Control.Monad.Except ( MonadIO, liftIO )
-import Control.Newtype.Generics ( ala )
-import qualified Data.Map as M
+import Control.Exception ( try )
+import Control.Monad.Except ( MonadIO, (>=>), liftIO )
 import Data.Monoid ( First (..) )
-import Graphics.HsExif ( ExifTag, ExifValue, dateTime, dateTimeDigitized,
-  dateTimeOriginal, parseFileExif )
+import Graphics.Exif ( Exif, fromFile, getTag )
+import System.IO.Error ( ioeGetErrorString, isUserError )
 
 import Photoname.Common ( SrcPath (..) )
 
 
-{-
-  Load EXIF information from filepath, or evaluate to Nothing
--}
 
+{- load Exif information from a filename, returning Nothing if libexif
+   encounters a NULL instead of raising an IO error.
+
+   This is somewhat bogus because of the error type that is used by
+   the EXIF library. We have to compare the error string to see if it
+   is the kind of user error that we expect. If we build against an
+   EXIF library that raises some other exception, then this build will
+   still succeed, and the exception will be propagated instead of
+   transformed into a handled photoname error.
+-}
+safeExif :: FilePath -> IO (Maybe Exif)
+safeExif = try . fromFile >=> either handleBadExif (return . Just)
+    where
+      isBadExif e =
+         ioeGetErrorString e == "mkExif: NULL" && isUserError e
+      handleBadExif e =
+         if isBadExif e then return Nothing else ioError e
+
+
+{- Get shoot date from the exif information. There are several tags 
+   potentially containing dates. Try them in a specific order until we
+   find one that has data.
+-}
 getExifDate :: MonadIO m => SrcPath -> m (Maybe String)
-getExifDate (SrcPath fp) = liftIO $ extractDate <$> parseFileExif fp
+getExifDate (SrcPath fp) = do
+  -- Get EXIF data from the image file, if present
+  mbExif <- liftIO $ safeExif fp
 
+  -- These are the EXIF tags we're interested in looking for, and in this order
+  let dateTagNames = ["DateTimeOriginal", "DateTimeDigitized", "DateTime"]
 
-{-
-  Extract the date from the passed in EXIF data, if we got any from the file.
-  Evaluate to Nothing if we can't locate a date at all.
--}
+  -- This code is constructing an ordered list of `getTag` actions for the
+  -- above tags in the EXIF data that will be performed by the First Semigroup
+  -- expression below. In the event the EXIF data is missing (Nothing), we
+  -- simply evaluate to an (m [])
+  lSearches <- maybe (pure []) (\e -> mapM (liftIO . getTag e) dateTagNames) mbExif
 
-extractDate :: Either String (M.Map ExifTag ExifValue) -> Maybe String
-extractDate (Left _) = Nothing
-extractDate (Right exifMap) =
-  -- Find the first date available in the Map
-  show <$> ala First foldMap (map (flip M.lookup exifMap)
-    [dateTimeOriginal, dateTimeDigitized, dateTime])
+  -- Now get the first one that doesn't fail
+  pure . getFirst . mconcat . map First $ lSearches
